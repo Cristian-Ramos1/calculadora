edd3f665957cee6bc0c6f3d9ea5670a227c906f2
Cristian <cristianramos92639@gmail.com>
2021-10-18 18:00:14 -0300

    agregue calduladora.js

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const sumar = require("./sumar.js");
+const restar = require("./restar.js");
+const dividir = require("./dividir")
+const multiplicar = require ("./multiplicar.js");
+
+console.log(sumar(3, 7))
+console.log(restar(3000, 200))
+console.log(dividir(1000000, 789))
+console.log(multiplicar(24,25))
